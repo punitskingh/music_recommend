ee327dfb592fd8147a46a6eeb08c9c2889314f33
Punit Singh <punitsskingh@gmail.com>
2025-03-12 18:16:45 +0530

    links

--- main.py
+++ main.py
@@ -13,6 +13,15 @@ client_credentials_manager = SpotifyClientCredentials(
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 
+def get_song_url(song_name):
+    results = sp.search(q=song_name, limit=1, type="track")
+    if results["tracks"]["items"]:
+        song_url = results["tracks"]["items"][0]["external_urls"]["spotify"]
+        return song_url
+    else:
+        return "#"
+
+
 def get_song_album_cover_url(song_name, artist_name):
     search_query = f"track:{song_name} artist:{artist_name}"
     results = sp.search(q=search_query, type="track")
@@ -32,6 +41,7 @@ def recommend(song):
     )
     recommended_music_names = []
     recommended_music_posters = []
+    recommended_music_link = []
     for i in distances[1:6]:
         # fetch the movie poster
         artist = music.iloc[i[0]].artist
@@ -39,8 +49,9 @@ def recommend(song):
             get_song_album_cover_url(music.iloc[i[0]].song, artist)
         )
         recommended_music_names.append(music.iloc[i[0]].song)
+        recommended_music_link.append(get_song_url(music.iloc[i[0]].song))
 
-    return recommended_music_names, recommended_music_posters
+    return recommended_music_names, recommended_music_posters, recommended_music_link
 
 
 st.header("Music Recommender System")
@@ -49,23 +60,58 @@ similarity = pickle.load(open("similarity.pkl", "rb"))
 
 music_list = music["song"].values
 selected_movie = st.selectbox("Type or select a song from the dropdown", music_list)
-
 if st.button("Show Recommendation"):
-    recommended_music_names, recommended_music_posters = recommend(selected_movie)
+    recommended_music_names, recommended_music_posters, recommended_music_links = (
+        recommend(selected_movie)
+    )
     col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
-        st.text(recommended_music_names[0])
         st.image(recommended_music_posters[0])
+        st.markdown(
+            f"""
+    <a href="{recommended_music_links[0]}" target="_blank" style="text-decoration: none; color: magneta; font-size: 15px;">
+        {recommended_music_names[0]}
+    </a>
+    """,
+            unsafe_allow_html=True,
+        )
     with col2:
-        st.text(recommended_music_names[1])
         st.image(recommended_music_posters[1])
-
+        st.markdown(
+            f"""
+    <a href="{recommended_music_links[1]}" target="_blank" style="text-decoration: none; color: magneta; font-size: 15px;">
+        {recommended_music_names[1]}
+    </a>
+    """,
+            unsafe_allow_html=True,
+        )
     with col3:
-        st.text(recommended_music_names[2])
         st.image(recommended_music_posters[2])
+        st.markdown(
+            f"""
+    <a href="{recommended_music_links[2]}" target="_blank" style="text-decoration: none; color: magneta; font-size: 15px;">
+        {recommended_music_names[2]}
+    </a>
+    """,
+            unsafe_allow_html=True,
+        )
     with col4:
-        st.text(recommended_music_names[3])
         st.image(recommended_music_posters[3])
+        st.markdown(
+            f"""
+    <a href="{recommended_music_links[3]}" target="_blank" style="text-decoration: none; color: magneta; font-size: 15px;">
+        {recommended_music_names[3]}
+    </a>
+    """,
+            unsafe_allow_html=True,
+        )
     with col5:
-        st.text(recommended_music_names[4])
         st.image(recommended_music_posters[4])
+        st.markdown(
+            f"""
+    <a href="{recommended_music_links[4]}" target="_blank" style="text-decoration: none; color: magneta; font-size: 15px;">
+        {recommended_music_names[4]}
+    </a>
+    """,
+            unsafe_allow_html=True,
+        )
